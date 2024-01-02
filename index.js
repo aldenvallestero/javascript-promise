1059652da1ca257108d0dce2eb57c2dc61247f5e
Alden Vallestero <alden.v@agtuary.com>
2024-01-02 19:47:04 +0800

    ♻️ Scalable & Straightforward Implementation

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 async function promise() {
-
   const expensiveCalculation1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Expensive Calculation 1');
-      resolve(1);
-    }, 10000);
+    setTimeout(resolve, 10000, 'expensiveCalculation1');
   });
   
   const expensiveCalculation2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Expensive Calculation 2');
-      reject(2);
-    }, 2000);
+    setTimeout(reject, 2000, 'expensiveCalculation2');
   });
   
   const expensiveCalculation3 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Expensive Calculation 3');
-      resolve(3);
-    }, 10000);
+    setTimeout(resolve, 10000, 'expensiveCalculation3');
   });
 
-  const start = new Date();
-
-  await Promise.all([
+  const promises = [
     expensiveCalculation1,
     expensiveCalculation2,
     expensiveCalculation3,
-  ]).then(result => {
+  ];
+
+  console.log('Start ...');
+  const start = new Date();
+
+  await Promise.all(promises).then(result => {
     console.info('Result:', result);
   }).catch(e => {
     console.error('Error:', e)
   });
 
   const end = new Date();
+  console.log('End ...');
 
   console.info(end - start);
 }
