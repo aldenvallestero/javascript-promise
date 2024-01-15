0ed7f13848f5f7c366c26fd1f11bd4ff898e8cdf
Alden Vallestero <alden.v@agtuary.com>
2024-01-15 11:30:21 +0800

    ♻️ Correct Execution Series

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 async function promise() {
-  const expensiveCalculation1 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 10000, 'expensiveCalculation1');
-  });
-  
-  const expensiveCalculation2 = new Promise((resolve, reject) => {
-    setTimeout(reject, 2000, 'expensiveCalculation2');
-  });
-  
-  const expensiveCalculation3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 10000, 'expensiveCalculation3');
-  });
-
-  const promises = [
-    expensiveCalculation1,
-    expensiveCalculation2,
-    expensiveCalculation3,
-  ];
 
   console.log('Start ...');
   const start = new Date();
 
-  await Promise.all(promises).then(result => {
-    console.info('Result:', result);
-  }).catch(e => {
-    console.error('Error:', e)
-  });
+  await Promise.all([
+
+    new Promise((resolve, reject) => {
+      setTimeout(resolve, 10000, 'expensiveCalculation1');
+      console.log(456)
+    }),
+
+    new Promise((resolve, reject) => {
+      setTimeout(reject, 2000, 'expensiveCalculation2');
+    }),
+
+    new Promise((resolve, reject) => {
+      setTimeout(resolve, 10000, 'expensiveCalculation3');
+    }),
+
+  ])
+
+    .then(result => {
+      console.info('Result:', result);
+    })
+    
+    .catch(e => {
+      console.error('Error:', e)
+    });
 
   const end = new Date();
   console.log('End ...');
@@ -32,4 +34,4 @@ async function promise() {
   console.info(end - start);
 }
 
-promise()
+promise();
