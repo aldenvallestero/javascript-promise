9c0b39d1f3a30a24f635fe13accee7e1f3e53299
Alden Vallestero <alden.v@agtuary.com>
2024-01-15 15:53:49 +0800

    ✨ Add Any & Race Promises

---
+++ promises/any-promise.js
@@ -0,0 +1,36 @@
+async function promise() {
+
+  console.log('Start ...');
+  const start = new Date();
+
+  await Promise.any([
+
+    new Promise((resolve, reject) => {
+      setTimeout(resolve, 3000, 'expensiveCalculation1');
+    }),
+
+    new Promise((resolve, reject) => {
+      setTimeout(reject, 2000, 'expensiveCalculation2');
+    }),
+
+    new Promise((resolve, reject) => {
+      setTimeout(resolve, 10000, 'expensiveCalculation3');
+    }),
+
+  ])
+
+    .then(result => {
+      console.info('Result:', result);
+    })
+    
+    .catch(e => {
+      console.error('Error:', e)
+    });
+
+  const end = new Date();
+  console.log('End ...');
+
+  console.info(end - start);
+}
+
+promise();
